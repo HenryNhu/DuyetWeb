57dda888a277731da87be56e02c6172b27a671aa
Quoc Anh Nhu <nhuquocanh.henry.work@gmail.com>
2025-09-11 11:29:29 +0700

    Add project files.

---
+++ DuyetWeb/MainPage.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DuyetWeb
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbBack = new System.Windows.Forms.ToolStripButton();
+            this.tsbForward = new System.Windows.Forms.ToolStripButton();
+            this.tsbReload = new System.Windows.Forms.ToolStripButton();
+            this.tsbHome = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 27);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 423);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbBack,
+            this.tsbForward,
+            this.tsbReload,
+            this.tsbHome});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbBack
+            // 
+            this.tsbBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbBack.Image = global::DuyetWeb.Properties.Resources.back_icon;
+            this.tsbBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBack.Name = "tsbBack";
+            this.tsbBack.Size = new System.Drawing.Size(29, 24);
+            this.tsbBack.Text = "Back";
+            this.tsbBack.Click += new System.EventHandler(this.tsbBack_Click);
+            // 
+            // tsbForward
+            // 
+            this.tsbForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbForward.Image = global::DuyetWeb.Properties.Resources.forward_icon;
+            this.tsbForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbForward.Name = "tsbForward";
+            this.tsbForward.Size = new System.Drawing.Size(29, 24);
+            this.tsbForward.Text = "Forward";
+            this.tsbForward.Click += new System.EventHandler(this.tsbForward_Click);
+            // 
+            // tsbReload
+            // 
+            this.tsbReload.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbReload.Image = global::DuyetWeb.Properties.Resources.reload_icon;
+            this.tsbReload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbReload.Name = "tsbReload";
+            this.tsbReload.Size = new System.Drawing.Size(29, 24);
+            this.tsbReload.Text = "Reload";
+            this.tsbReload.Click += new System.EventHandler(this.tsbReload_Click);
+            // 
+            // tsbHome
+            // 
+            this.tsbHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbHome.Image = global::DuyetWeb.Properties.Resources.home_icon;
+            this.tsbHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbHome.Name = "tsbHome";
+            this.tsbHome.Size = new System.Drawing.Size(29, 24);
+            this.tsbHome.Text = "toolStripButton1";
+            this.tsbHome.Click += new System.EventHandler(this.tsbHome_Click);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainPage";
+            this.Text = "DuyetWeb";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbBack;
+        private System.Windows.Forms.ToolStripButton tsbForward;
+        private System.Windows.Forms.ToolStripButton tsbReload;
+        private System.Windows.Forms.ToolStripButton tsbHome;
+    }
+}
+
---
+++ DuyetWeb/MainPage.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuyetWeb
+{
+    public partial class MainPage : Form
+    {
+        #region ==== Config ====
+        // Trang chủ mặc định
+        private const string CONFIG_HOMEPAGE_URL = "https://wmsv6.tkelog.com";
+
+        // Chu kỳ quét tiến trình (ms)
+        private const int CONFIG_PROCESS_SCAN_INTERVAL_MS = 2000;
+        #endregion
+
+        #region ==== Members ====
+        // Danh sách domain cho phép
+        private readonly HashSet<string> m_dicAllowedDomains = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "wmsv6.tkelog.com",
+        };
+
+        // Danh sách trình duyệt block
+        private readonly HashSet<string> m_dicBrowsersToBlock = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            // "msedge", // Edge gốc
+            "chrome",
+            "firefox",
+            "opera",
+            "brave"
+        };
+
+        private Timer m_objProcessMonitorTimer;
+        private int m_iCurrentProcessId;
+        #endregion
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Load += MainPage_Load;
+            this.FormClosed += MainPage_FormClosed;
+        }
+
+        #region ==== Lifecycle ====
+        private async void MainPage_Load(object sender, EventArgs e)
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.NavigationStarting += WebView_NavigationStarting;
+            webView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            webView.CoreWebView2.Navigate(CONFIG_HOMEPAGE_URL);
+            m_iCurrentProcessId = Process.GetCurrentProcess().Id;
+            m_objProcessMonitorTimer = new Timer
+            {
+                Interval = CONFIG_PROCESS_SCAN_INTERVAL_MS
+            };
+            m_objProcessMonitorTimer.Tick += ProcessMonitorTimer_Tick;
+            m_objProcessMonitorTimer.Start();
+        }
+
+        private void MainPage_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (m_objProcessMonitorTimer != null)
+            {
+                m_objProcessMonitorTimer.Stop();
+                m_objProcessMonitorTimer.Tick -= ProcessMonitorTimer_Tick;
+                m_objProcessMonitorTimer.Dispose();
+                m_objProcessMonitorTimer = null;
+            }
+
+            if (webView != null)
+            {
+                if (webView.CoreWebView2 != null)
+                {
+                    webView.NavigationStarting -= WebView_NavigationStarting;
+                    webView.CoreWebView2.NewWindowRequested -= CoreWebView2_NewWindowRequested;
+                }
+                webView.Dispose();
+            }
+        }
+        #endregion
+
+        #region ==== Guards ====
+
+        // Hàm điều hướng
+        private void WebView_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            if (Uri.TryCreate(e.Uri, UriKind.Absolute, out Uri v_uri))
+            {
+                bool v_isAllowed = IsHostAllowed(v_uri.Host);
+                if (!v_isAllowed)
+                {
+                    e.Cancel = true;
+                    MessageBox.Show(
+                        $"Truy cập vào trang web '{v_uri.Host}' đã bị chặn.",
+                        "Truy cập bị từ chối",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private bool IsHostAllowed(string p_strHost)
+        {
+            if (string.IsNullOrWhiteSpace(p_strHost)) return false;
+
+            // Cho phép nếu host khớp chính xác hoặc là subdomain của domain được phép
+            foreach (var v_domain in m_dicAllowedDomains)
+            {
+                if (p_strHost.Equals(v_domain, StringComparison.OrdinalIgnoreCase))
+                    return true;
+
+                if (p_strHost.EndsWith("." + v_domain, StringComparison.OrdinalIgnoreCase))
+                    return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region ==== External Browser Blocker ====
+        //  Định kỳ quét & kill các trình duyệt không cho phép
+        private void ProcessMonitorTimer_Tick(object sender, EventArgs e)
+        {
+            Process[] v_arrAllProcesses;
+            try
+            {
+                v_arrAllProcesses = Process.GetProcesses();
+            }
+            catch
+            {
+                return;
+            }
+
+            foreach (var v_process in v_arrAllProcesses)
+            {
+                if (v_process.Id == m_iCurrentProcessId) continue;
+
+                string v_nameSafe;
+                try
+                {
+                    v_nameSafe = v_process.ProcessName; 
+                }
+                catch
+                {
+                    continue;
+                }
+
+                if (m_dicBrowsersToBlock.Contains(v_nameSafe))
+                {
+                    try
+                    {
+                        v_process.Kill();
+                    }
+                    catch
+                    {
+
+                    }
+                    finally
+                    {
+                        try { v_process.Dispose(); } catch { /* ignore */ }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region ==== Toolbar Actions ====
+        private void tsbBack_Click(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+        }
+
+        private void tsbForward_Click(object sender, EventArgs e)
+        {
+            if (webView.CanGoForward)
+            {
+                webView.GoForward();
+            }
+        }
+
+        private void tsbReload_Click(object sender, EventArgs e)
+        {
+            webView?.Reload();
+        }
+
+        private void tsbHome_Click(object sender, EventArgs e)
+        {
+            if (webView?.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(CONFIG_HOMEPAGE_URL);
+            }
+        }
+        #endregion
+    }
+}
